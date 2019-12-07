341a2db59763f4f120053c53de664a8976c655e1
paul-cruz <poljet12@gmail.com>
2019-12-06 20:37:51 -0600

    PropType Shape added

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
+import PropTypes from 'prop-types';
 import './styles.css';
 
 const WeatherData = ({ data }) => {
@@ -12,5 +13,14 @@ const WeatherData = ({ data }) => {
         </div>
     );
 }
+
+WeatherData.propTypes = {
+    data : PropTypes.shape({
+        temperature : PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
